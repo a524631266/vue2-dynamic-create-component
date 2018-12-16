9bc496b4df4b9da64a3d1443c442aea95adf9904
zhangll <524631266@qq.com>
2018-12-17 00:20:06 +0800

    use extend method

--- dcc/src/views/DynamicExtend.vue
+++ dcc/src/views/DynamicExtend.vue
@@ -10,12 +10,13 @@
             <div class="card-text">
                 <div>1.动态切换区</div>
                 <textarea name="" id="" class="col" rows="10">动态地绑定到它的 is 特性，我们让多个组件可以使用同一个挂载点，并动态切换。如果把切换出去的组件保留在内存中，可以保留它的状态或避免重新渲染。为此可以添加一个 keep-alive 指令参数.通过切换后的时间可以看到keep-alive的作用
-                </textarea>
+                </textarea> 
             </div>
             <div class="card-text">
                 <div>2.动态添加组件</div>
                 <textarea name="" id="" class="col" rows="10">
-                    利用vue中的v-for语法糖特性，通过监听当前组件内部的组件名列表对象。当触发按钮（添加组件名）的时候，在列表中添加指定的名字就会动态添加组件
+                    使用vue.extend 继承扩展构造函数来动态创建子组件，本应用没有进行任何扩展，所以，相当于是原组件的一份class copy,
+                    同时在父组件中维护用的子组件，并在子组件的props的相关变量做监听，同时使用全局函数set方法，同步更新数据
                 </textarea>
             </div>
         </div>
@@ -32,11 +33,16 @@
             <ul class="nav nav-pills card-header-pills">
                 <li class="nav-item"><a class="nav-link" :class="currentView===third?'active':''" href="javascript:void(0);" @click="pop();">删除最后一个元素</a></li>
             </ul>
+            <div class="input-group">
+                <input class="input-group-append form-control" type="text" v-model="ownnames[first]" placeholder="firstname"/>
+                <input class=" form-control" type="text" v-model="ownnames[second]" placeholder="secondname"/>
+                <input class="input-group-append form-control" type="text" v-model="ownnames[third]" placeholder="thirdname"/>
+            </div>
         </div>
         <div class="card-body bg-light">
             <h6 class="text-primary">1.切换区</h6>
             <keep-alive>
-            <component :is="currentView" :name="currentView"></component>
+            <component :is="currentView" :name="ownnames[currentView]"></component>
             </keep-alive>
         </div>
         <div class="card-body bg-primary" >
@@ -73,29 +79,17 @@ export default class Dynamic extends Vue {
     private third = "Third";
     private deletecomponents: Set<any> = new Set();
     private componetsinstancelist = {[this.first]: First,[this.second]: Second,[this.third]: Third};
+    private ownnames = {[this.first]: "name1",[this.second]: "name2",[this.third]: "name3"};
+    private childrenVue: {[x: string]: Vue[]} = {[this.first]: [],[this.second]: [],[this.third]: []};
     public mounted() {
-        console.log("1111111111");
         const firstConstruct = Vue.extend(First);
         // tslint:disable-next-line:new-parens
         const firstinst =  (new firstConstruct({
-            data:()=> {
-                return {
-                    name2:"11"
-                };
-            },
-            props:{
-                name:{
-                    default: [this.name]
-                }
-            },
-            watch:{
-                name:(old)=> {
-                    console.log("22222");
-                    this.name = old;
-                },
-            }
         })).$mount();
+        this.$set(firstinst,"name",this.ownnames[this.first]);
+        // Vue.set(firstinst,"name",this.ownnames[this.first]);
         (document.getElementById("addarea") as any).appendChild(firstinst.$el as any);
+        this.childrenVue[this.first].push(firstinst);
     }
     @Emit()
     private changename() {
@@ -107,17 +101,31 @@ export default class Dynamic extends Vue {
         const construct = Vue.extend(this.componetsinstancelist[tabText]);
         // tslint:disable-next-line:new-parens
         const instacne = (new construct({
-            props:{
-                name2:{
-                    default:["22222"]
-                },
-            }
         })).$mount();
+        Vue.set(instacne,"name",this.ownnames[tabText]);
         (document.getElementById("addarea") as any).appendChild(instacne.$el as any);
+        this.childrenVue[tabText].push(instacne);
     }
     @Emit()
     // tslint:disable-next-line:no-empty
     private pop() {
+        const vuelist = this.childrenVue[this.currentView];
+        const lastvue = vuelist[vuelist.length-1];
+        (document.getElementById('manual2') as any).appendChild(lastvue.$el as any);
+    }
+    @Watch("ownnames",{deep: true })
+    private updateChildren(newVal: {[x: string]: string}, oldVal: {[x: string]: string}) {
+        // Vue.set(this.childrenVue[0],"name","222222");
+        Object.keys(oldVal).forEach(
+            (value) => {
+                // console.log(oldVal[value],newVal[value]);
+                this.childrenVue[value].forEach(
+                    (vueinstance) => {
+                        Vue.set(vueinstance,"name",newVal[value]);
+                    }
+                );
+            }
+        );
     }
 }
 </script>
